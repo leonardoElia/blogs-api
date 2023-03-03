0642cef3af73d39b03231b18b5653bdbb4b7868c
Leonardo Elias Souza de Oliverira Meireles <leonardoelias802@gmail.com>
2023-03-03 17:31:28 -0300

    fazendo pedido 2

---
+++ src/models/User.js
@@ -0,0 +1,22 @@
+/**
+ * 
+ * @param {import('sequelize').Sequelize} sequelize 
+ * @param {import('sequelize').DataTypes} DataTypes 
+ */
+
+module.exports = (sequelize, DataTypes) => {
+    const user = sequelize.define('User', {
+        id:{ type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+        displayName:{type: DataTypes.STRING},
+        email:{type: DataTypes.STRING},
+        password:{type: DataTypes.STRING},
+        image:{type: DataTypes.STRING},
+    }, 
+    
+    {
+     timestamps:false,
+     tableName:'users',
+     underscored:true,
+    })
+ return user;
+}
